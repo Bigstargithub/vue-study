bb676ec066d702e11df5c634d722a91af1eeb03c
Bigstargithub <qufquf2004@hanmail.net>
2021-11-23 11:11:42 +0900

    vue 공부중

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+  <h2 @click="$emit('click')">
+    {{ msg }}
+  </h2>
+  <img
+    src="~assets/logo.png"
+    alt="HEROPY" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: { type: string }
+  },
+  emits: ['click']
+}
+</script>
+
+<style scoped lang="scss">
+  @import "~/scss/main";
+
+  h2 {
+    font-size:22px;
+    color:$color--primary;
+    & + img {
+      width:120px;
+    }
+  }
+</style>
